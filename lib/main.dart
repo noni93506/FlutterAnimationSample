21772b05797099d9bcd9963863102004bba674a9
yevhen.mediakov <yevhen.mediakov@stfalcon.com>
2023-12-28 17:54:39 +0200

    update sprite position fix margin == null

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  double positionX = 0.0;
-  double positionY = 0.0;
+  late Offset _offset;
   bool playAnim = false;
   late AnimationController _backgroundController;
   late Animation<Color?> _colorAnimation;
 
   @override
   void initState() {
+    _offset = const Offset(0, 0);
     super.initState();
     _backgroundController = AnimationController(
       duration: const Duration(seconds: 2),
@@ -61,52 +61,68 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     return Scaffold(
       body: Stack(children: [
         AnimatedBuilder(
-          animation: _colorAnimation,
-          builder: (context, child) {
-            return Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              color: _colorAnimation.value,
-              child: child,
+        animation: _colorAnimation,
+        builder: (context, child) {
+          return Container(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            color: _colorAnimation.value,
+            child: child,
+          );
+        },
+        child: const SizedBox(),
+      ),
+      Positioned.fill(
+        child: FloatingBubbles.alwaysRepeating(
+          noOfBubbles: 50,
+          colorsOfBubbles: const [
+            Colors.white,
+            Colors.lightBlueAccent,
+          ],
+          sizeFactor: 0.2,
+          opacity: 70,
+          speed: BubbleSpeed.slow,
+          paintingStyle: PaintingStyle.fill,
+          shape: BubbleShape.circle, //This is the default
+        ),
+      ),
+      GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            _offset = Offset(
+              (_offset.dx + details.delta.dx).clamp(0.0, MediaQuery
+                  .of(context)
+                  .size
+                  .width - 50.0),
+              (_offset.dy + details.delta.dy).clamp(0.0, MediaQuery
+                  .of(context)
+                  .size
+                  .height - 50.0),
             );
-          },
-          child: const SizedBox(),
+          });
+        },
+        child: Container(
+        width: 100,
+        height: 150,
+        margin: EdgeInsets.only(left: _offset.dx, top: _offset.dy),
+        child: const Sprite(
+          stepTime: 200,
+          axis: Axis.horizontal,
+          imagePath: "assets/images/sprite.png",
+          size: Size(150, 230),
+          amount: 10,
         ),
-        Positioned.fill(
-          child: FloatingBubbles.alwaysRepeating(
-            noOfBubbles: 50,
-            colorsOfBubbles: const [
-              Colors.white,
-              Colors.lightBlueAccent,
-            ],
-            sizeFactor: 0.2,
-            opacity: 70,
-            speed: BubbleSpeed.slow,
-            paintingStyle: PaintingStyle.fill,
-            shape: BubbleShape.circle, //This is the default
-          ),
-        ),
-        GestureDetector(
-          onPanUpdate: (details) {
-            setState(() {
-              positionX += details.delta.dx;
-              positionY += details.delta.dy;
-            });
-          },
-          child: Container(
-            width: 100,
-            height: 150,
-            margin: EdgeInsets.only(left: positionX, top: positionY),
-            child: const Sprite(
-              stepTime: 200,
-              axis: Axis.horizontal,
-              imagePath: "assets/images/sprite.png",
-              size: Size(150, 230),
-              amount: 10,
-            ),
-          ),
-        ),
-      ]),
+      ),
+
+    ),]
+    )
+    ,
     );
   }
 }
